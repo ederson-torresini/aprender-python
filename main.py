18f993088f7a9649ee3a80d7a98b671cf1282433
Ederson Torresini <etorresini@ifsc.edu.br>
2025-09-15 19:12:38 -0300

    Preparar código para VSCode

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
 from random import randint
 
-print(randint(1, 100))
+while True:
+    operador = randint(1, 1000)
+    print("O operador é:", operador)
+
+    operacao = randint(1, 5)
+
+    operando = randint(1, 1000)
+    print("O operando é:", operando)
+
+    if operacao == 1:
+        resultado = operador + operando
+        print("A operacao é soma")
+    elif operacao == 2:
+        resultado = operador - operando
+        print("A operacao é subtracao")
+    elif operacao == 3:
+        resultado = operador * operando
+        print("A operacao é multiplicacao")
+    elif operacao == 4:
+        resultado = operador / operando
+        print("A operacao é divisao")
+
+    print("Qual o resultado?")
+
+    entrada = input()
+
+    if entrada == str(resultado):
+        print("Parabéns!")
+    else:
+        print("Seu bocó, a resposta era:", resultado)
